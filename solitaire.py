e38a42a7565a83aaae22282bf1bc98e3564244a2
Daryl DeFord <daryl.r.deford@gmail.com>
2019-04-27 17:41:09 -0400

    Update solitaire.py

--- solitaire.py
+++ solitaire.py
@@ -1,8 +1,10 @@
+import matplotlib.pyplot as plt
 @interact
 def war(n=input_box(label='Number of deals: ',default=1000),cards=input_box(label='Number of cards to win: ',default=3), deck=input_box(label='Size of deck: ',default=52),auto_update=False):	
 
 	wins=0
 	winp=[]
+	game=[]
 
 	for i in range(n):
 		#print(i)
@@ -14,7 +16,19 @@ def war(n=input_box(label='Number of deals: ',default=1000),cards=input_box(labe
 		if b==a:
 			#print('yay')
 			wins+=1
+			game.append(1)
+		else:
+			game.append(0)
 		winp.append(wins/(i+1))
                
 	list_plot(winp).show()
+	plt.figure()
+	ax = plt.gca()
+	ax.set_yticks([0,1])
+	ax.set_yticklabels([':(',':)'])
+	plt.plot(game,'o',markersize=2)
+	plt.title('Win?')
+	plt.xlabel('Game #')
+    
+	plt.show()
 	pretty_print('Current Win Percentage:', (wins/n).n())
